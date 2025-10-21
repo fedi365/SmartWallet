2dd1e5af554766f800330946f37c3b6541d67a7f
Fedi Mejri <fedi.mejri@dh-ss.com>
2025-10-21 18:00:35 +0100

    change in db file

--- db.js
+++ db.js
@@ -1,21 +1,24 @@
 import dotenv from "dotenv";
-import {neon} from "@neondatabase/serverless";
+import { neon } from "@neondatabase/serverless";
 
 dotenv.config();
 
 export const sql = neon(process.env.DataBase_URL);
-export async function initDB(){
-    try {
-        await sql`CREATE TABLE IF NOT EXISTS transactions(
-  id SERIAL PRIMARY KEY,
-            user_id INT NOT NULL,
-            title VARCHAR(255) NOT NULL,
-            amount INT NOT NULL,
-            category VARCHAR(255) NOT NULL,
-            Created_at Date NOT NULL DEFAULT CURRENT_DATE
-                  )`
 
-    }catch (error){
-        console.log(error)
+export async function initDB() {
+    try {
+        await sql`
+            CREATE TABLE IF NOT EXISTS transactions (
+                                                        id SERIAL PRIMARY KEY,
+                                                        user_id VARCHAR(255) NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                amount INT NOT NULL,
+                category VARCHAR(255) NOT NULL,
+                created_at DATE NOT NULL DEFAULT CURRENT_DATE
+                )
+        `;
+        console.log("Table 'transactions' ready !");
+    } catch (error) {
+        console.error("Erreur lors de la création de la table :", error);
     }
 }
